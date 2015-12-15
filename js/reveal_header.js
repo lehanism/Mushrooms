8ae7e8209f98be00127ea7f92cb6308c782bc604
LukeLehane <lukelehane@live.com>
2015-12-15 20:55:53 +0000

    update hidden header

--- js/reveal_header.js
+++ js/reveal_header.js
@@ -1,11 +1,15 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-    if (document.body.scrollTop > 20) {
+window.onscroll = function() {
+    if (document.body.scrollTop > 500) {
         document.getElementById("change_opacity").style.opacity="100";
         document.getElementById("change_padding").style.padding="5px 0 5px 10%";
     } else {
         document.getElementById("change_opacity").style.opacity="0";
         document.getElementById("change_padding").style.padding="5px 0 5px 30%";
+    }
+
+    if (document.body.scrollTop > 700) {
+        document.getElementById("reveal_htext").style.opacity="100";
+    } else {
+        document.getElementById("reveal_htext").style.opacity="0";
     }
 }
